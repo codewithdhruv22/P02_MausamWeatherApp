cba7498990c09a201e155ceba0a7e849f719a269
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2021-03-28 10:51:06 +0530

    Code Of Video No #2

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mausam/Acitivity/home.dart';
+import 'package:mausam/Acitivity/location.dart';
+import 'package:mausam/Acitivity/loading.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
