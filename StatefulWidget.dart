daaaa26d635e136aa713420c1276d87e94e6e1f6
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2021-04-10 08:46:14 +0530

    Video No.4 Code

---
+++ StatefulWidget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int counter = 1;
+  @override
+  void initState() {
+    super.initState();
+    print("This is a init state");
+  }
+
+  @override
+  void setState(fn) {
+    // TODO: implement setState
+    super.setState(fn);
+    print("Set state called");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("Widget Destroyed");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Activity"),
+      ),
+      body:
+          Column(
+            children: <Widget>[
+              FloatingActionButton(
+                onPressed: () => setState(() {
+                  counter += 1;
+                }),
+              ),
+              Text("$counter")
+            ],
+          )
+
+
+
+    );
+  }
+}
+
