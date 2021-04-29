df0705e3ba8745c6ab78aecb55d68440de259ad1
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2021-04-29 22:17:06 +0530

    Code Of Video No. 20

---
+++ home.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  void initState() {
+    super.initState();
+    print("This is a init state");
+  }
+
+  @override
+  void setState(fn) {
+    // TODO: implement setState
+    super.setState(fn);
+    print("Set state called");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("Widget Destroyed");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var city_name = ["Mumbai", "Delhi","Chennai", "Dhar", "Indore", "London"];
+    final _random = new Random();
+    var city = city_name[_random.nextInt(city_name.length)];
+
+    return Scaffold(
+
+      body: SafeArea(
+
+        child: Container(
+          child: Column(
+            children: [
+              Container( //Search Wala Container
+
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                margin: EdgeInsets.symmetric(horizontal: 24,vertical: 20),
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(24)
+                ),
+                child: Row(
+
+                  children: [
+                   GestureDetector(
+                     onTap: () {
+                       print("Search Me");
+                     },
+                     child: Container(child: Icon(Icons.search,color: Colors.blueAccent,),margin: EdgeInsets.fromLTRB(3, 0, 7, 0),),
+                   ),
+                    Expanded(
+                      child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search $city"
+                      ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
